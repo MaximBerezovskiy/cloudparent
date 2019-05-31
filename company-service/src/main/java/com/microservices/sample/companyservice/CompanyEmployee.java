d1ef5829ddf3a113f54708a9b40d7a85b22a7347
Maxim Berezovskiy <maxim.berezovskiy@yahoo.com>
2019-06-01 01:09:03 +0300

    Added feign support for getting company employees. Added entity.

---
+++ company-service/src/main/java/com/microservices/sample/companyservice/CompanyEmployee.java
@@ -0,0 +1,38 @@
+package com.microservices.sample.companyservice;
+
+
+class CompanyEmployee {
+
+    public CompanyEmployee() {
+    }
+
+    private Long id;
+
+    private String name;
+
+    private Long companyId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+}
